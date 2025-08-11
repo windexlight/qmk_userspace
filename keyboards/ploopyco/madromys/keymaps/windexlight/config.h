9aaa30d9bc437ddb8590e1c4a2ad8d0a44284e4f
windexlight <windexlight@gmail.com>
2025-08-10 20:24:25 -0700

    Untested thoughts on improving hires scrolling

--- keyboards/ploopyco/madromys/keymaps/windexlight/config.h
+++ keyboards/ploopyco/madromys/keymaps/windexlight/config.h
@@ -1,11 +1,14 @@
 #pragma once
 
-// #define POINTING_DEVICE_HIRES_SCROLL_ENABLE 0
-// #define POINTING_DEVICE_HIRES_SCROLL_MULTIPLIER 15
+#define POINTING_DEVICE_HIRES_SCROLL_ENABLE 0
+#define POINTING_DEVICE_HIRES_SCROLL_MULTIPLIER 15 // Maybe play with this
+#define WHEEL_EXTENDED_REPORT // Necessary to send wheel reports with 16-bit values to avoid overflowing
+
+// TO DO - Also throttle the rate at which scroll messages are sent in ploopyco.c
 
 #define PLOOPY_DRAGSCROLL_MOMENTARY
-#define PLOOPY_DRAGSCROLL_DIVISOR_H 4.0
-#define PLOOPY_DRAGSCROLL_DIVISOR_V 4.0
+#define PLOOPY_DRAGSCROLL_DIVISOR_H 120.0
+#define PLOOPY_DRAGSCROLL_DIVISOR_V 80.0
 #define PLOOPY_DRAGSCROLL_INVERT
 
 #define PLOOPY_DRAGSCROLL_SCROLLOCK
