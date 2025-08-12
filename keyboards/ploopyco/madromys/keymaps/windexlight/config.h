5813f4fcf6256ba22bc4f57c6aa0c2ea17e7891e
windexlight <windexlight@gmail.com>
2025-08-11 17:30:19 -0700

    Hires scrolling

--- keyboards/ploopyco/madromys/keymaps/windexlight/config.h
+++ keyboards/ploopyco/madromys/keymaps/windexlight/config.h
@@ -1,14 +1,12 @@
 #pragma once
 
-#define POINTING_DEVICE_HIRES_SCROLL_ENABLE 0
+#define POINTING_DEVICE_HIRES_SCROLL_ENABLE
 #define POINTING_DEVICE_HIRES_SCROLL_MULTIPLIER 15 // Maybe play with this
 #define WHEEL_EXTENDED_REPORT // Necessary to send wheel reports with 16-bit values to avoid overflowing
 
-// TO DO - Also throttle the rate at which scroll messages are sent in ploopyco.c
-
 #define PLOOPY_DRAGSCROLL_MOMENTARY
-#define PLOOPY_DRAGSCROLL_DIVISOR_H 120.0
-#define PLOOPY_DRAGSCROLL_DIVISOR_V 80.0
+#define PLOOPY_DRAGSCROLL_DIVISOR_H 4.0 //120.0
+#define PLOOPY_DRAGSCROLL_DIVISOR_V 4.0 //80.0
 #define PLOOPY_DRAGSCROLL_INVERT
 
 #define PLOOPY_DRAGSCROLL_SCROLLOCK
