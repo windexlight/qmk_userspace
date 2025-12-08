d6e51b317bbc2cc36a2c0717640406d7e0e3e128
windexlight <windexlight@gmail.com>
2025-12-07 16:49:04 -0800

    Experiment with pinky shift

--- keyboards/cantor/keymaps/windexlight/keymap.c
+++ keyboards/cantor/keymaps/windexlight/keymap.c
@@ -370,7 +370,7 @@ uint8_t USAGE2KEYCODE(uint16_t usage) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_DH] = LAYOUT_split_3x6_3(
         KC_ESC,   KC_Q,  KC_W,  KC_F,  KC_P,  KC_B,      KC_J,  KC_L,  KC_U,    KC_Y,   KC_QUOT, KC_BSPC,
-        TG(_MSE), KC_A,  KC_R,  KC_S,  KC_T,  KC_G,      KC_M,  KC_N,  KC_E,    KC_I,   KC_O,    KC_ENT,
+        KC_LSFT, KC_A,  KC_R,  KC_S,  KC_T,  KC_G,      KC_M,  KC_N,  KC_E,    KC_I,   KC_O,    KC_RSFT,
         CLR_OSM,  KC_Z,  KC_X,  KC_C,  KC_D,  KC_V,      KC_K,  KC_H,  KC_COMM, KC_DOT, KC_SLSH, KC_DEL,
                    EX_MO(_NUM), EX_MO(_EXT), KC_LSFT,      EX_MO(_SYM), KC_SPC, EX_MO(_FNC)
     ),
