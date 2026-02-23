dc610d2aae77a7487c3a6a742777fcd90dfc5095
windexlight <windexlight@gmail.com>
2026-02-22 17:36:08 -0800

    More LazyVim

--- keyboards/cantor/keymaps/windexlight/keymap.c
+++ keyboards/cantor/keymaps/windexlight/keymap.c
@@ -631,8 +631,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_NUM_FNC] = LAYOUT_split_3x6_3(
         KC_F1,   KC_F2, KC_F3, KC_F4,  KC_F5,  KC_F6,       KC_EQL,  KC_7, KC_8, KC_9, KC_0,    KC_TRNS,
-        KC_TRNS, _LALT, _LGUI,  _LSFT,  _LCTL, QK_LEAD,     KC_ASTR, KC_4, KC_5, KC_6, KC_DOT,  KC_TRNS,
-        KC_F7,   KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,      KC_TILD, KC_1, KC_2, KC_3, KC_SLSH, KC_TRNS,
+        KC_TRNS, _LALT, _LGUI,  _LSFT,  _LCTL, QK_LEAD,     KC_ASTR, KC_4, KC_5, KC_6, KC_DOT,  LCTL(KC_Y),
+        KC_F7,   KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,      KC_TILD, KC_1, KC_2, KC_3, KC_SLSH, LCTL(KC_E),
                             KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [_MSE] = LAYOUT_split_3x6_3(
