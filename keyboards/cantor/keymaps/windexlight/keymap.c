bec727d4efc9bebaa2d0a8cfec29392aa8def529
windexlight <windexlight@gmail.com>
2026-08-06 20:15:19 -0700

    Swap thumb keys a bit

--- keyboards/cantor/keymaps/windexlight/keymap.c
+++ keyboards/cantor/keymaps/windexlight/keymap.c
@@ -738,13 +738,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         SK_DS,        KC_V,       KC_M,       KC_L,         KC_C,       KC_P,         KC_B,        MAGIC,       KC_U,       KC_O,       KC_Q,  TD(TD_CAPS),
         KC_ENT,  _ALT(KC_S), _CTL(KC_T), _SFT(KC_R), _SYM_R(KC_D), _NAV(KC_Y),   _NUM(KC_F), _SYM_L(KC_N), _SFT(KC_E), _CTL(KC_A), _ALT(KC_I),    KC_BSPC,
         KC_TAB,  _GUI(KC_X),      KC_K,       KC_J,         KC_G,       KC_W,         KC_Z,    _FUN(KC_H),    KC_COMM,     KC_DOT, _GUI(KC_QUOT), QK_LEAD,
-                                                        KC_DEL, KC_SPC, KC_ESC,      QK_REP, KC_UNDS, KC_MINS
+                                                       KC_DEL, QK_REP, KC_ESC,      KC_UNDS, KC_SPC, KC_MINS
     ),
     [_QWERTY_NVIM] = LAYOUT_split_3x6_3(
         SK_DS,        KC_Q,       KC_W,       KC_E,         KC_R,       KC_T,         KC_Y,         KC_U,       KC_I,       KC_O,       KC_P,     LCTL(KC_U),
         KC_ENT,  _ALT(KC_A), _CTL(KC_S), _SFT(KC_D), _SYM_R(KC_F), _NAV(KC_G),   _NUM(KC_H), _SYM_L(KC_J), _SFT(KC_K), _CTL(KC_L), _ALT(KC_SCLN), KC_UNDS,
         KC_TAB,  _GUI(KC_Z),      KC_X,       KC_C,         KC_V,       KC_B,         KC_N,    _FUN(KC_M),    KC_COMM,     KC_DOT, _GUI(KC_SLSH), LCTL(KC_D),
-                                                       KC_LBRC, KC_SPC, KC_ESC,      OSL_NUM, TSL_NUM, KC_RBRC
+                                                      KC_LBRC, KC_SPC, KC_ESC,      OSL_NUM, TSL_NUM, KC_RBRC
     ),
     [_NAV_LAYER] = LAYOUT_split_3x6_3(
         KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_NO, KC_TRNS,
